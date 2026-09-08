8b7b93162c4c5b1ee7380a0bc65b7cfd91eae8c0
karansharma5348 <karan.18563@sakec.ac.in>
2026-09-09 00:04:58 +0530

    Time: 0 ms (100.00%) | Memory: 46.5 MB (16.78%) - LeetSync

---
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.java
@@ -0,0 +1,92 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverse(ListNode head, int times) {
+
+        ListNode curr = head;
+        ListNode prev = null;
+
+        while (times > 0) {
+
+            ListNode next = curr.next;
+
+            curr.next = prev;
+
+            prev = curr;
+            curr = next;
+
+            times--;
+        }
+
+        return prev;
+    }
+    public ListNode reverseKGroup(ListNode head, int k) {
+         if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode left = head;
+        ListNode prevleft = null;
+        ListNode res = null;
+
+        int size = k;
+
+        while (left != null) {
+
+            // Find the second node
+            ListNode right = left;
+
+            for (int i = 0; i < size - 1; i++) {
+
+                if (right == null) {
+                    break;
+                }
+
+                right = right.next;
+            }
+
+            // Not enough nodes for a pair
+            if (right == null) {
+
+                if (prevleft != null) {
+                    prevleft.next = left;
+                }
+
+                break;
+            }
+
+            // Save node after the pair
+            ListNode nextleft = right.next;
+
+            // Reverse the pair
+            ListNode newHead = reverse(left, size);
+
+            // left is now the TAIL of reversed pair
+            left.next = nextleft;
+
+            // Connect previous pair
+            if (prevleft != null) {
+                prevleft.next = newHead;
+            }
+
+            // Store answer
+            if (res == null) {
+                res = newHead;
+            }
+
+            // Move to next pair
+            prevleft = left;
+            left = nextleft;
+        }
+
+        return res;
+    }
+}
